dcb69fa124c544b2f2cda1a320be76931c331c34
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-05-13 21:19:09 +0530

    Updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './App.css'
 import Weather from './Components/Weather/Weather'
 
 function App() {
+    const videoRef = useRef()
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 0.6
+        }
+    }, [])
+
     return (
         <div className='app'>
-            <video className='video' src='cloud.mp4' autoPlay muted loop></video>
+            <video className='video' ref={videoRef} src='cloud.mp4' autoPlay muted loop></video>
             <Weather />
         </div>
     )
